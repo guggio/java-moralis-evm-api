de8de4232da4cd7ac11a1f30bf2cc05e77c86569
guggio <sebastian.guggisberg@gmail.com>
2023-02-27 22:57:49 +0100

    refactored the final layers of the api with a new base class that takes over most of the work

---
+++ src/main/java/io/moralis/evm/api/ExecutingApi.java
@@ -0,0 +1,59 @@
+package io.moralis.evm.api;
+
+import com.google.gson.Gson;
+import io.moralis.evm.api.exception.ApiErrorMessage;
+import io.moralis.evm.api.exception.ConnectionException;
+import io.moralis.evm.api.queryparams.QueryParams;
+import io.moralis.evm.api.queryparams.QueryParamsManager;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public abstract class ExecutingApi extends BaseApiImpl {
+
+  private final QueryParamsManager queryParamsManager;
+
+  protected ExecutingApi(BaseApi wrappedApi) {
+    super(wrappedApi);
+    queryParamsManager = new QueryParamsManager();
+  }
+
+  @Override
+  protected final String getUrlPath() {
+    return getUrlPartBeforeQueryParams() + getQueryParams();
+  }
+
+  protected abstract String getUrlPartBeforeQueryParams();
+
+  private String getQueryParams() {
+    return queryParamsManager.getParams();
+  }
+
+  protected final void addQuery(QueryParams queryParams) {
+    queryParamsManager.add(queryParams);
+  }
+
+  protected final <T> T get(Class<T> clazz) {
+    HttpClient httpClient = HttpClient.newHttpClient();
+
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(buildUrl()))
+        .header("X-API-KEY", getApiKey())
+        .GET()
+        .build();
+
+    try {
+      HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+      if (response.statusCode() >= HttpURLConnection.HTTP_BAD_REQUEST) {
+        throw new ConnectionException(response.statusCode(), new Gson().fromJson(response.body(), ApiErrorMessage.class));
+      }
+      return new Gson().fromJson(response.body(), clazz);
+    } catch (IOException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
--- src/main/java/io/moralis/evm/api/token/transfer/TokenTransferWalletApiImpl.java
+++ src/main/java/io/moralis/evm/api/token/transfer/TokenTransferWalletApiImpl.java
@@ -1,110 +1,79 @@
 package io.moralis.evm.api.token.transfer;
 
-import com.google.gson.Gson;
 import io.moralis.evm.api.BaseApi;
-import io.moralis.evm.api.BaseApiImpl;
-import io.moralis.evm.api.exception.ApiErrorMessage;
-import io.moralis.evm.api.exception.ConnectionException;
+import io.moralis.evm.api.ExecutingApi;
 import io.moralis.evm.api.queryparams.QueryParams;
-import io.moralis.evm.api.queryparams.QueryParamsManager;
 import io.moralis.evm.core.Address;
 import io.moralis.evm.core.Chain;
 import io.moralis.evm.model.Erc20TransactionCollection;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 
-public class TokenTransferWalletApiImpl extends BaseApiImpl implements TokenTransferWalletApi {
+public class TokenTransferWalletApiImpl extends ExecutingApi implements TokenTransferWalletApi {
 
   private final Address wallet;
-  private final QueryParamsManager queryParamsManager;
 
   public TokenTransferWalletApiImpl(BaseApi wrappedApi, Address wallet) {
     super(wrappedApi);
     this.wallet = wallet;
-    queryParamsManager = new QueryParamsManager();
   }
 
   @Override
-  protected String getUrlPath() {
-    return "/" + wallet.getAddress() + "/erc20/transfers" + getParams();
+  protected String getUrlPartBeforeQueryParams() {
+    return "/" + wallet.getAddress() + "/erc20/transfers";
   }
 
-  private String getParams() {
-    return queryParamsManager.getParams();
-  }
 
   @Override
   public TokenTransferWalletApi chain(Chain chain) {
-    queryParamsManager.add(QueryParams.chain(chain));
+    addQuery(QueryParams.chain(chain));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi fromBlock(long blockNumber) {
-    queryParamsManager.add(QueryParams.fromBlock(blockNumber));
+    addQuery(QueryParams.fromBlock(blockNumber));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi toBlock(long blockNumber) {
-    queryParamsManager.add(QueryParams.toBlock(blockNumber));
+    addQuery(QueryParams.toBlock(blockNumber));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi fromDate(LocalDateTime fromDate) {
-    queryParamsManager.add(QueryParams.fromDate(fromDate));
+    addQuery(QueryParams.fromDate(fromDate));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi toDate(LocalDateTime toDate) {
-    queryParamsManager.add(QueryParams.toDate(toDate));
+    addQuery(QueryParams.toDate(toDate));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi pageSize(int pageSize) {
-    queryParamsManager.add(QueryParams.pageSize(pageSize));
+    addQuery(QueryParams.pageSize(pageSize));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi disableTotal(boolean disableTotal) {
-    queryParamsManager.add(QueryParams.disableTotal(disableTotal));
+    addQuery(QueryParams.disableTotal(disableTotal));
     return this;
   }
 
   @Override
   public TokenTransferWalletApi cursor(String cursor) {
-    queryParamsManager.add(QueryParams.cursor(cursor));
+    addQuery(QueryParams.cursor(cursor));
     return this;
   }
 
   @Override
   public Erc20TransactionCollection get() {
-    HttpClient httpClient = HttpClient.newHttpClient();
-
-    HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(buildUrl()))
-        .header("X-API-KEY", getApiKey())
-        .GET()
-        .build();
-
-    try {
-      HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      if (response.statusCode() >= HttpURLConnection.HTTP_BAD_REQUEST) {
-        throw new ConnectionException(response.statusCode(), new Gson().fromJson(response.body(), ApiErrorMessage.class));
-      }
-      return new Gson().fromJson(response.body(), Erc20TransactionCollection.class);
-    } catch (IOException | InterruptedException e) {
-      throw new RuntimeException(e);
-    }
+    return get(Erc20TransactionCollection.class);
   }
 }
